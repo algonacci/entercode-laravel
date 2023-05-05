1017493b91cf818f750788f446e5558a790d605c
Eric Julianto <algonacci1@gmail.com>
2023-05-05 19:51:16 +0700

    make role seeder and assign it to user/admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,6 +23,6 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
-        ]);
+        ])->assignRole("admin");
     }
 }
